010227859df7aee70bbd577f826b0e11e2878055
JUAN DIEGO SALINAS CONTRERAS <juan.salinasc@aviatur.com.co>
2026-02-27 14:58:56 -0500

    Agregar Ejemplo de Admin y Usuario (MockUps)

---
+++ Foro Militar/Controllers/AdminController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+public class AdminController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Foro Militar/Controllers/HomeUsuarioController.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Mvc;
+
+public class HomeUsuarioController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
+
